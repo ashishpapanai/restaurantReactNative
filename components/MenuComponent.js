293935ac853c65a72d487b80b2edac060347404f
Ashish Papanai <ashishpapanai00@gmail.com>
2020-07-09 10:13:52 +0530

    Components Part-2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import Dishdetail from './DishdetailComponent';
+import { DISHES } from '../shared/dishes';
 
 function Menu(props) {
 
-    const renderMenuItem = ({item, index}) => {
+    const renderMenuItem = ({ item, index }) => {
 
         return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    leftAvatar={{ source: require('./images/uthappizza.png')}}
-                  />
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                onPress={() => props.onPress(item.id)}
+                leftAvatar={{ source: require('./images/uthappizza.png') }}
+            />
         );
     };
 
     return (
-            <FlatList 
-                data={props.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
-                />
+        <FlatList
+            data={props.dishes}
+            renderItem={renderMenuItem}
+            keyExtractor={item => item.id.toString()}
+        />
     );
 }
 
